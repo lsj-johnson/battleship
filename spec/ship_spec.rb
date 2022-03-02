5c1c625b43d00c5cd850712708dd5b866a9f6145
Lu Johnson <lucasjohnson0613@gmail.com>
2022-03-02 16:13:27 -0700

    ship class complete

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -5,9 +5,32 @@ RSpec.describe Ship do
     @cruiser = Ship.new('Cruiser', 3)
   end
 
-  it 'is a ship' do
+  it 'can create object' do
     expect(@cruiser).to be_a(Ship)
+  end
+
+  it 'has attributes' do
     expect(@cruiser.name).to eq('Cruiser')
     expect(@cruiser.length).to eq(3)
+    expect(@cruiser.health).to eq(3)
+  end
+
+  it 'has sunk status' do
+    expect(@cruiser.sunk?).to be false
+  end
+
+  it 'can take hits and be sunk' do
+    @cruiser.hit
+
+    expect(@cruiser.health).to eq(2)
+
+    @cruiser.hit
+
+    expect(@cruiser.sunk?).to be false
+    expect(@cruiser.health).to eq(1)
+
+    @cruiser.hit
+
+    expect(@cruiser.sunk?).to be true
   end
 end
