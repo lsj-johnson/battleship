e48ab34f694bbb6d15fc509d7bf08ddb23a22daf
Lu Johnson <lucasjohnson0613@gmail.com>
2022-03-04 16:30:37 -0700

    add #render to show status of cell

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,10 @@
 class Cell
-  attr_reader :coordinate, :ship
+  attr_reader :coordinate, :ship, :fired_upon
 
   def initialize(coordinate, ship = nil)
     @coordinate = coordinate
     @ship = ship
+    @fired_upon = false
   end
 
   def empty?
@@ -15,16 +16,27 @@ class Cell
   end
 
   def fired_upon?
-    fired_upon = false
-    if not empty?
-      fired_upon = @ship.health < @ship.length
-    end
-    fired_upon
+    @fired_upon
   end
 
   def fire_upon
+    @fired_upon = true
     if not empty?
       @ship.hit
     end
+  end
+
+  def render(show=false)
+    render = '.'
+    if show and not empty? and not @fired_upon
+      render = 'S'
+    elsif empty? and @fired_upon
+      render = 'M'
+    elsif not empty? and @ship.sunk?
+      render = 'X'
+    elsif not empty? and @fired_upon
+      render = 'H'
+    end
+    render
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -40,5 +40,36 @@ RSpec.describe Cell do
       expect(@cell.ship.health).to eq(2)
       expect(@cell.fired_upon?).to be true
     end
+
+    it 'can #render when cell empty' do
+      expect(@cell.render).to eq('.')
+
+      @cell.fire_upon
+
+      expect(@cell.render).to eq('M')
+    end
+
+    before  :each do
+      @cell_2 = Cell.new('C3')
+      @cell_2.place_ship(@cruiser)
+    end
+
+    it 'can #render when show is true' do
+      expect(@cell_2.render).to eq('.')
+      expect(@cell_2.render(true)).to eq('S')
+    end
+
+    it 'can #render when cell is not empty' do
+      @cell_2.fire_upon
+
+      expect(@cell_2.render).to eq('H')
+      expect(@cruiser.sunk?).to be false
+
+      @cruiser.hit
+      @cruiser.hit
+
+      expect(@cruiser.sunk?).to be true
+      expect(@cell_2.render).to eq('X')
+    end
   end
 end
